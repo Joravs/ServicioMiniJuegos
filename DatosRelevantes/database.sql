cb495ac951d19f0152c6d0f18473874fdcc01c03
Joravs <jordyl.reyesl@gmail.com>
2025-02-21 18:20:01 +0100

    Changes

--- DatosRelevantes/database.sql
+++ DatosRelevantes/database.sql
@@ -3,28 +3,28 @@ CREATE DATABASE lilgames;
 use lilgames;
 CREATE TABLE juegos
 (
-  info VARCHAR(50) NOT NULL,
-  nombreJuego VARCHAR(25) NOT NULL,
   idJuego INT NOT NULL auto_increment,
+  nombreJuego VARCHAR(25) NOT NULL,
+  info VARCHAR(50) NOT NULL,
   PRIMARY KEY (idJuego)
 );
 
 CREATE TABLE usuarios
 (
+  idUsuario INT NOT NULL auto_increment,
+  nombre VARCHAR(50) NOT NULL,
+  username VARCHAR(25) NOT NULL,
   passwd VARCHAR(255) NOT NULL,
   nivel INT NOT NULL,
-  idUsuario INT NOT NULL auto_increment,
-  username VARCHAR(25) NOT NULL,
-  nombre VARCHAR(50) NOT NULL,
   PRIMARY KEY (idUsuario),
   UNIQUE (username)
 );
 
 CREATE TABLE stats
 (
-  partidasJugadas INT NOT NULL,
   idUsuario INT NOT NULL,
   idJuego INT NOT NULL,
+  partidasJugadas INT NOT NULL,
   PRIMARY KEY (idUsuario, idJuego),
   FOREIGN KEY (idUsuario) REFERENCES usuarios(idUsuario) ON DELETE CASCADE ON UPDATE CASCADE,
   FOREIGN KEY (idJuego) REFERENCES juegos(idJuego) ON DELETE CASCADE ON UPDATE CASCADE
@@ -41,18 +41,18 @@ CREATE TABLE juegosFavs
 
 CREATE TABLE statsPoints
 (
-  recordPoints INT NOT NULL,
   idUsuario INT NOT NULL,
   idJuego INT NOT NULL,
+  recordPoints INT NOT NULL,
   PRIMARY KEY (idUsuario, idJuego),
   FOREIGN KEY (idUsuario, idJuego) REFERENCES stats(idUsuario, idJuego) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE statsTime
 (
-  recordTime time NOT NULL,
   idUsuario INT NOT NULL,
   idJuego INT NOT NULL,
+  recordTime time NOT NULL,
   PRIMARY KEY (idUsuario, idJuego),
   FOREIGN KEY (idUsuario, idJuego) REFERENCES stats(idUsuario, idJuego) ON DELETE CASCADE ON UPDATE CASCADE
 );
