fc0c7b1c9c6575b342300d9d8456c86b5bec1dc5
Joravs <jordyl.reyesl@gmail.com>
2025-02-24 17:56:11 +0000

    Mejoras

--- lilgames/resources/views/welcome.blade.php
+++ lilgames/resources/views/welcome.blade.php
@@ -1,6 +1,5 @@
 <x-layout>
     <div class="row">
-        @if(Auth::check())
             @php
                 $color = 'transparent';
                 $idJuego = []; // Inicializar el array
@@ -19,9 +18,11 @@
                     $color = in_array($game->idJuego, $idJuego) ? 'yellow' : 'transparent'; // Asignar color según la existencia en $idJuego
                 @endphp
                 <div class="card col m-2 border-white text-center">
+                    @if(Auth::check())
                     <svg id="{{ $game->idJuego }}" name="{{ $game->idJuego }}" class="star" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="64px">
                         <polygon points="50,5 63,38 98,38 69,59 82,92 50,71 18,92 31,59 2,38 37,38" fill="{{ $color }}" stroke="black" stroke-width="3"/>
                     </svg>
+                    @endif
                     <a href="{{ route($game->nombreJuego) }}">
                         <img class="card-img-top juegos align-self-center img-fluid" src="{{ asset('src/'.$game->nombreJuego.'.png') }}" alt="{{ $game->nombreJuego }} Logo"/>
                         <div class="card-body bg-white">
@@ -31,7 +32,6 @@
                     </a>
                 </div>
             @endforeach
-        @endif
     </div>
     @vite(['resources/js/validations/star.js'])
 </x-layout>
